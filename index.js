13554f18b929863fe92bfaf8bc506c52317b8b02
johnson <johnsonnjichi@gmail.com>
2023-03-16 19:27:47 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+
+
+function receivesAFunction(spy) {
+    spy()
+    
+}
+function returnsANamedFunction(){
+  return  receivesAFunction;
+    
+}
+
+function returnsAnAnonymousFunction () {
+   return function(){
+
+   }
+
+}
+
+
+    
+
+
+
+
+
+
+
+
